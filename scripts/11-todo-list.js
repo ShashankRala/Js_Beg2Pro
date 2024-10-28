c4bc5757871a6e66261c8862a55ceba2521fe49b
ShashankRala <ralashashankyadav0402@gmail.com>
2024-10-29 01:14:01 +0530

    11 part 1

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,24 @@
-const todoList=[];
+const todoList=['make dinner','fill water'];
+
+renderTodoList();
+
+
+function renderTodoList(){
+  let todoListHTML = '';
+
+  for(let i=0;i< todoList.length;i++)
+  {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;//thi is called Generating the HTML 
+    todoListHTML += html;
+  }
+
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list').innerHTML=todoListHTML;
+
+}
+
 
 function addTodo() {
   const inputElement =  document.querySelector('.js-name-input');
@@ -7,4 +27,6 @@ function addTodo() {
   todoList.push(name);
   console.log(todoList);
   inputElement.value='';//this makes textbox empty after adding
+
+  renderTodoList();
 }
